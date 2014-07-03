b07f83ce184d62e8aed566a844ed7e2e23bfb214
dutchmo <gregory_matous@yahoo.com>
2014-07-03 12:08:25 -0500

    Removed action bar stuff to be consistent with other activities.

--- Recycle Austin/src/main/java/com/austindroids/recycleaustin/activities/ScheduleActivity.java
+++ Recycle Austin/src/main/java/com/austindroids/recycleaustin/activities/ScheduleActivity.java
@@ -1,17 +1,17 @@
-package com.austindroids.recycleaustin;
+package com.austindroids.recycleaustin.activities;
 
+import android.app.Activity;
 import android.graphics.Color;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.austindroids.recycleaustin.R;
 
-public class ScheduleActivity extends ActionBarActivity {
+
+public class ScheduleActivity extends Activity {
 
 
     // constants for Intent string extra names
@@ -78,26 +78,6 @@ public class ScheduleActivity extends ActionBarActivity {
             str = "[value not passed to Intent]";
         }
         return str;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.schedule, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
